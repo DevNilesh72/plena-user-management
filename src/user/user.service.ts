d6d71d13924eed6639b31480495ccf8082306c4d
Nilesh <das.nilesh72@gmail.com>
2024-06-21 19:57:32 +0530

    User CURD operations with prisma

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,37 +1,71 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Prisma } from '@prisma/client';
 import { SearchUserDto } from './dto/search-user.dto';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return createUserDto;
+  constructor(private prisma: PrismaService) {}
+  create(data: Prisma.UserCreateInput) {
+    return this.prisma.user.create({
+      data,
+    });
   }
 
   findAll() {
-    return `This action returns all user`;
+    return this.prisma.user.findMany({});
   }
 
-  search(serchUserDto: SearchUserDto) {
-    if(Object.keys(serchUserDto).length === 0){
-      throw new HttpException({
-        statusCode: HttpStatus.BAD_REQUEST,
-        error: 'Serch parameter not provided',
-      }, HttpStatus.BAD_REQUEST);
+  search(searchUserDto: SearchUserDto) {
+    if (Object.keys(searchUserDto).length === 0) {
+      throw new HttpException(
+        {
+          statusCode: HttpStatus.BAD_REQUEST,
+          error: 'Serch parameter not provided',
+        },
+        HttpStatus.BAD_REQUEST,
+      );
     }
-    return serchUserDto;
+
+    return this.prisma.user.findMany({
+      where: {
+        OR: [
+          {
+            username: {
+              contains: searchUserDto.username,
+            },
+          },
+          {
+            age: {
+              gte: searchUserDto.minage || 0,
+              lte: searchUserDto.maxage || 1000,
+            },
+          },
+        ],
+      },
+    });
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} user`;
+    return this.prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return {id, ...updateUserDto};
+  update(id: number, data: Prisma.UserUpdateInput) {
+    return this.prisma.user.update({
+      where: {
+        id,
+      },
+      data,
+    });
   }
 
   remove(id: number) {
-    return `This action removes a #${id} user`;
+    return this.prisma.user.delete({
+      where: { id },
+    });
   }
 }
